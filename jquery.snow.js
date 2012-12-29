883e42b03807cd3dae3aadef2ff520e59c4ad989
Foxel <foxel@quickfox.ru>
2012-12-29 12:39:06 +0700

    improved options, moved to fixed positioning

--- jquery.snow.js
+++ jquery.snow.js
@@ -15,53 +15,46 @@
  * @example $.fn.snow({ maxSize: 200, newOn: 1000 });
  */
 (function($){
-	
-	$.fn.snow = function(options){
-	
-			var $flake 			= $('<div class="flake" />').css({'position': 'absolute', 'top': '-50px'}).html('&#10052;'),
-				documentHeight 	= $(document).height(),
-				documentWidth	= $(document).width(),
-				defaults		= {
-									minSize		: 10,
-									maxSize		: 20,
-									newOn		: 500,
-									flakeColor	: "#FFFFFF"
-								},
-				options			= $.extend({}, defaults, options);
-				
-			
-			var interval		= setInterval( function(){
-				var startPositionLeft 	= Math.random() * documentWidth - 100,
-				 	startOpacity		= 0.5 + Math.random(),
-					sizeFlake			= options.minSize + Math.random() * options.maxSize,
-					endPositionTop		= documentHeight - defaults.maxSize - 40,
-					endPositionLeft		= startPositionLeft - 100 + Math.random() * 200,
-					durationFall		= documentHeight * 10 + Math.random() * 5000;
-				$flake
-					.clone()
-					.appendTo('body')
-					.css(
-						{
-							left: startPositionLeft,
-							opacity: startOpacity,
-							'font-size': sizeFlake,
-							color: options.flakeColor
-						}
-					)
-					.animate(
-						{
-							top: endPositionTop,
-							left: endPositionLeft,
-							opacity: 0.2
-						},
-						durationFall,
-						'linear',
-						function() {
-							$(this).remove()
-						}
-					);
-			}, options.newOn);
-	
-	};
-	
+    
+    $.fn.snow = function(options){
+        var $flake       = $('<div class="flake" />').css({'position': 'fixed', 'top': '-50px', 'z-index': 1000}).html('&#10052;'),
+            windowHeight = $(window).height(),
+            windowWidth  = $(window).width(),
+            defaults     = {
+                minSize    : 10,
+                maxSize    : 20,
+                newOn      : 1000,
+                flakeColor : "#FFFFFF",
+                shadowColor: 'gray'
+            };
+        options = $.extend({}, defaults, options);
+            
+        
+        var interval = setInterval( function(){
+            var startPositionLeft = Math.random() * windowWidth - 100,
+                startOpacity      = 0.5 + Math.random(),
+                sizeFlake         = options.minSize + Math.random() * options.maxSize,
+                endPositionTop    = windowHeight - defaults.maxSize - 40,
+                endPositionLeft   = startPositionLeft - 100 + Math.random() * 200,
+                durationFall      = windowHeight * 10 + Math.random() * 5000;
+
+            $flake
+                .clone()
+                .appendTo('body')
+                .css({
+                    'left': startPositionLeft,
+                    'opacity': startOpacity,
+                    'font-size': sizeFlake,
+                    'color': options.flakeColor,
+                    'text-shadow': '1px 1px 2px ' + options.shadowColor
+                })
+                .animate({
+                    top: endPositionTop,
+                    left: endPositionLeft,
+                    opacity: 0.2
+                }, durationFall, 'linear', function() {
+                    $(this).remove()
+                });
+        }, options.newOn);
+    };
 })(jQuery);
